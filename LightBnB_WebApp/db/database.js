f953d4df2204e23e3000e3b251788e8baef756f3
Daniel Minogue <danielcminogue@gmail.com>
2024-04-22 13:52:25 -0600

    Add sql query to getAllReservations function in database.js.

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -114,7 +114,7 @@ const getAllReservations = function (guest_id, limit = 10) {
             AVG(property_reviews.rating) AS average_rating
         FROM reservations
         JOIN properties ON reservations.property_id = properties.id
-        LEFT JOIN property_reviews ON properties.id = property_reviews.property_id
+        JOIN property_reviews ON properties.id = property_reviews.property_id
         WHERE reservations.guest_id = $1
         GROUP BY properties.id, reservations.id
         ORDER BY reservations.start_date
