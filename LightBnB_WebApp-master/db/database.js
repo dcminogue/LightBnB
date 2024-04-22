b9f6e59775713627eb08c6e4b9a345201f37832a
Daniel Minogue <danielcminogue@gmail.com>
2024-04-22 12:24:58 -0600

    Add sql queries to update getUserWithEmail, getUserWithId, and addUser functions in database.js file.

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -14,21 +14,33 @@ const properties = require("./json/properties.json");
 const users = require("./json/users.json");
 
 /// Users
-
 /**
- * Get a single user from the database given their email.
- * @param {String} email The email of the user.
- * @return {Promise<{}>} A promise to the user.
- */
+ 
+//  * Get a single user from the database given their email.
+//  * @param {String} email The email of the user.
+//  * @return {Promise<{}>} A promise to the user.
+//  */
+
 const getUserWithEmail = function (email) {
-    let resolvedUser = null;
-    for (const userId in users) {
-        const user = users[userId];
-        if (user && user.email.toLowerCase() === email.toLowerCase()) {
-            resolvedUser = user;
-        }
-    }
-    return Promise.resolve(resolvedUser);
+    const query = `
+      SELECT *
+      FROM users
+      WHERE LOWER(email) = LOWER($1);
+    `;
+
+    return pool
+        .query(query, [email])
+        .then(res => {
+            if (res.rows.length === 0) {
+                return null; // User not found
+            } else {
+                return res.rows[0]; // Return the first user found
+            }
+        })
+        .catch(err => {
+            console.error("Error executing query", err);
+            throw err; // Rethrow the error
+        });
 };
 
 /**
@@ -37,19 +49,47 @@ const getUserWithEmail = function (email) {
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function (id) {
-    return Promise.resolve(users[id]);
+    const query = `
+      SELECT *
+      FROM users
+      WHERE id = $1;
+    `;
+
+    return pool
+        .query(query, [id])
+        .then(res => {
+            if (res.rows.length === 0) {
+                return null; // User not found
+            } else {
+                return res.rows[0]; // Return the first user found
+            }
+        })
+        .catch(err => {
+            console.error("Error executing query", err);
+            return null; // Return null on error
+        });
 };
 
-/**
- * Add a new user to the database.
- * @param {{name: string, password: string, email: string}} user
- * @return {Promise<{}>} A promise to the user.
- */
 const addUser = function (user) {
-    const userId = Object.keys(users).length + 1;
-    user.id = userId;
-    users[userId] = user;
-    return Promise.resolve(user);
+    const query = `
+      INSERT INTO users (name, email, password)
+      VALUES ($1, $2, $3)
+      RETURNING *;
+    `;
+
+    return pool
+        .query(query, [user.name, user.email, user.password])
+        .then(res => {
+            if (res.rows.length === 0) {
+                throw new Error("Failed to add user"); // Throw error if user was not added
+            } else {
+                return res.rows[0]; // Return the newly added user
+            }
+        })
+        .catch(err => {
+            console.error("Error executing query", err);
+            throw err; // throw the error
+        });
 };
 
 /// Reservations
