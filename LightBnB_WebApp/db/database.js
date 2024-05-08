5d53c15a6a15986e1de290a936911b825492c613
dcminogue <danielcminogue@gmail.com>
2024-05-07 23:28:01 -0600

    Add an sql query to allow user to add a new property to the database.

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -64,7 +64,7 @@ const getUserWithId = function (id) {
             }
         })
         .catch(err => {
-            throw err; // throw the error
+            throw err; // Rethrow the error
         });
 };
 
@@ -85,7 +85,7 @@ const addUser = function (user) {
             }
         })
         .catch(err => {
-            throw err; // throw the error
+            throw err; // Rethrow the error
         });
 };
 
@@ -127,7 +127,7 @@ const getAllReservations = function (guest_id, limit = 10) {
             }
         })
         .catch(err => {
-            throw err; // throw the error
+            throw err; // Rethrow the error
         });
 };
 
@@ -224,24 +224,50 @@ const getAllProperties = (options, limit = 10) => {
         });
 };
 
-// {
-//     const limitedProperties = {};
-//     for (let i = 1; i <= limit; i++) {
-//         limitedProperties[i] = properties[i];
-//     }
-//     return Promise.resolve(limitedProperties);
-// };
-
 /**
  * Add a property to the database
  * @param {{}} property An object containing all of the property details.
  * @return {Promise<{}>} A promise to the property.
  */
+
 const addProperty = function (property) {
-    const propertyId = Object.keys(properties).length + 1;
-    property.id = propertyId;
-    properties[propertyId] = property;
-    return Promise.resolve(property);
+    const query = `
+        INSERT INTO properties 
+        (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night,
+        street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms) 
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) 
+        RETURNING *`;
+
+    const values = [
+        property.owner_id,
+        property.title,
+        property.description,
+        property.thumbnail_photo_url,
+        property.cover_photo_url,
+        property.cost_per_night,
+        property.street,
+        property.city,
+        property.province,
+        property.post_code,
+        property.country,
+        property.parking_spaces,
+        property.number_of_bathrooms,
+        property.number_of_bedrooms,
+    ];
+
+    return pool
+        .query(query, values)
+        .then(res => {
+            if (res.rows.length === 0) {
+                throw new Error("Failed to add property."); // Throw error if property not added
+            } else {
+                // Return the saved property
+                return res.rows[0];
+            }
+        })
+        .catch(err => {
+            throw err; // Rethrow the error.
+        });
 };
 
 module.exports = {
