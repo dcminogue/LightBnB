47d1c1cf25d9702898fce4052794930825aac173
dcminogue <danielcminogue@gmail.com>
2024-05-09 13:40:41 -0600

    Refactor getAllProperties function.

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -149,7 +149,7 @@ const getAllProperties = function (options, limit = 10) {
             properties.*, 
             AVG(property_reviews.rating) AS average_rating
         FROM properties
-        LEFT JOIN property_reviews ON properties.id = property_reviews.property_id`;
+        INNER JOIN property_reviews ON properties.id = property_reviews.property_id`;
 
     if (options.city) {
         queryParams.push(`%${options.city}%`);
@@ -195,20 +195,13 @@ const getAllProperties = function (options, limit = 10) {
         queryString += ` cost_per_night <= $${queryParams.length}`;
     }
 
-    if (options.minimum_rating) {
-        if (queryParams.length === 0) {
-            queryString += ` WHERE`;
-        } else {
-            queryString += ` AND`;
-        }
-        queryString += ` property_reviews.rating >= $${queryParams.length + 1}`;
-        queryParams.push(options.minimum_rating);
-    }
-
     queryString += `
-        ORDER BY average_rating DESC, cost_per_night
-        LIMIT $${queryParams.length + 1};`;
+        GROUP BY properties.id
+        HAVING AVG(property_reviews.rating) >= $${queryParams.length + 1}
+        ORDER BY cost_per_night
+        LIMIT $${queryParams.length + 2};`;
 
+    queryParams.push(options.minimum_rating);
     queryParams.push(limit);
 
     return pool
